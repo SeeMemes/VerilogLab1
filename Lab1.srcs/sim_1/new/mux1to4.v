47d5462434ec95865635890fa9ba412fc9b49221
SeeMemes <provotorov-02@mail.ru>
2022-03-30 17:22:33 +0300

    v1.1

--- Lab1.srcs/sim_1/new/mux1to4.v
+++ Lab1.srcs/sim_1/new/mux1to4.v
@@ -1,27 +1,33 @@
 `timescale 1ns / 1ps
 
 module mux1to4(
-    input A,
-    input B,
-    input C,
-    input D,
+    input Ai,
+    input Bi,
+    input Ci,
+    input Di,
     output Y,
     input S1,
     input S2
     );
     
     wire nS1, nS2, nY;
+    wire A, B, C, D;
     wire [ 3 : 0 ] nvar_tmp;
     wire [ 1 : 0 ] nres1;
     wire [ 1 : 0 ] nres2;
         
+    assign A = Ai;
+    assign B = Ci;
+    assign C = Bi;
+    assign D = Di;
+    
     nor( nS1, S1, S1 );   
     nor( nS2, S2, S2 );
     
     nor( nvar_tmp[ 0 ], A, S1 );
     nor( nvar_tmp[ 1 ], B, nS1 );
-    nor( nvar_tmp[ 2 ], C, nS1 );
-    nor( nvar_tmp[ 3 ], D, S1 );
+    nor( nvar_tmp[ 2 ], C, S1 );
+    nor( nvar_tmp[ 3 ], D, nS1 );
         
     nor ( nres1 [ 0 ], nvar_tmp [ 0 ], nvar_tmp [ 1 ] );
     nor ( nres1 [ 1 ], nvar_tmp [ 2 ], nvar_tmp [ 3 ] );
--- Lab1.srcs/sim_1/new/mux_test.v
+++ Lab1.srcs/sim_1/new/mux_test.v
@@ -6,10 +6,10 @@ module mux_test;
     wire Y_out;
     
     mux1to4 mux_test1 (
-        .A(Ai),
-        .B(Bi),
-        .C(Ci),
-        .D(Di),
+        .Ai(Ai),
+        .Bi(Bi),
+        .Ci(Ci),
+        .Di(Di),
         .Y(Y_out),
         .S1(S1i),
         .S2(S2i)
@@ -17,29 +17,29 @@ module mux_test;
     
     integer i;
     reg [ 5 : 0 ] test_val;
-    reg expected_val;
+    reg [ 3 : 0 ] expected_val;
         
     initial begin
             
         for ( i = 0; i < 64; i = i + 1 ) begin
             
             test_val = i;
-            S1i = test_val[ 0 ];
-            S2i = test_val[ 1 ];
-            Ai = test_val[ 2 ];
-            Bi = test_val[ 3 ];
-            Ci = test_val[ 4 ];
-            Di = test_val[ 5 ];
-            expected_val = S1i & Bi & Ci | S1i & ~S2i & Bi | ~S1i & Ai & Di | ~S1i & ~S2i & Ai | ~S1i & S2i & Di | S1i & S2i & Ci ;
+            S1i = test_val[ 4 ];
+            S2i = test_val[ 5 ];
+            Ai = test_val[ 0 ];
+            Bi = test_val[ 1 ];
+            Ci = test_val[ 2 ];
+            Di = test_val[ 3 ];
+            
                 
             #10
                 
             if ( 
-                Y_out == expected_val
+                Y_out == test_val[ {test_val[4] , test_val[5]} ]
             ) begin
                 $display( "The demux output is correct!!! S1i=%b, S2i = %b, Ai = %b, Bi = %b, Ci = %b, Di = %b, Y_out = %b", S1i, S2i, Ai, Bi, Ci, Di, Y_out );
             end else begin
-                $display( "The demux output is incorrect!!! S1i=%b, S2i = %b, Ai = %b, Bi = %b, Ci = %b, Di = %b, Y_out = %b, expected_val = %b", S1i, S2i, Ai, Bi, Ci, Di, Y_out, expected_val ); 
+                $display( "The demux output is incorrect!!! S1i=%b, S2i = %b, Ai = %b, Bi = %b, Ci = %b, Di = %b, Y_out = %b, expected_val = %b", S1i, S2i, Ai, Bi, Ci, Di, Y_out, test_val[ {test_val[4] , test_val[5]} ] ); 
             end
         end
             
